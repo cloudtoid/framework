873398a5f56291f9a5ffb5d234d0c08ce6e1b71c
Pedram Rezaei <pedramr@microsoft.com>
2020-04-12 23:06:48 -0700

    one more helper method

--- src/Cloudtoid.Framework/String/StringBuilderExtensions.cs
+++ src/Cloudtoid.Framework/String/StringBuilderExtensions.cs
@@ -98,6 +98,17 @@
         public static StringBuilder AppendFormatInvariant(this StringBuilder builder, string format, params object[] args)
             => CheckValue(builder, nameof(builder)).AppendFormat(CultureInfo.InvariantCulture, format, args);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static StringBuilder AppendIfNotEmpty(this StringBuilder builder, char value)
+        {
+            CheckValue(builder, nameof(builder));
+
+            if (builder.Length > 0)
+                builder.Append(value);
+
+            return builder;
+        }
+
         public static StringBuilder Append<TItem>(this StringBuilder builder, IEnumerable<TItem> items, string? separator = null)
         {
             CheckValue(builder, nameof(builder));
