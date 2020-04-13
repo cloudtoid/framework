421147652485c091abf3804180a8f3e1f49a94d5
Pedram Rezaei <pedramr@microsoft.com>
2020-04-12 21:29:50 -0700

    Adding an extension method

---
+++ src/Cloudtoid.Framework/Http/HttpStatusCodeExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Cloudtoid
+{
+    using System.Diagnostics;
+    using System.Net;
+
+    [DebuggerStepThrough]
+    public static class HttpStatusCodeExtensions
+    {
+        public static bool IsSuccessStatusCode(this HttpStatusCode statusCode)
+        {
+            var code = (int)statusCode;
+            return code >= 200 && code <= 299;
+        }
+    }
+}
---
+++ tests/Cloudtoid.Framework.UnitTests/Http/HttpStatusCodeTests.cs
@@ -0,0 +1,34 @@
+﻿namespace Cloudtoid.Framework.UnitTests
+{
+    using System;
+    using System.Linq;
+    using System.Net;
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public sealed class HttpStatusCodeTests
+    {
+        [TestMethod]
+        public void IsSuccessStatusCode_Between200And299_IsSuccess()
+        {
+            ((int[])Enum.GetValues(typeof(HttpStatusCode)))
+                .Where(v => v >= 200 && v < 300)
+                .Cast<HttpStatusCode>()
+                .All(c => c.IsSuccessStatusCode())
+                .Should()
+                .BeTrue();
+        }
+
+        [TestMethod]
+        public void IsSuccessStatusCode_NotBetween200And299_IsNotSuccess()
+        {
+            ((int[])Enum.GetValues(typeof(HttpStatusCode)))
+                .Where(v => v < 200 && v >= 300)
+                .Cast<HttpStatusCode>()
+                .All(c => !c.IsSuccessStatusCode())
+                .Should()
+                .BeTrue();
+        }
+    }
+}
