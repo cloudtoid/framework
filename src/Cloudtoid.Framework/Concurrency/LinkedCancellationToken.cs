bdf2673e9321d5834236a9882a8d14ff612e3380
Pedram Rezaei <pedramr@microsoft.com>
2020-08-21 22:46:00 -0700

    A new pool of linked cancellation tokens

---
+++ src/Cloudtoid.Framework/Concurrency/LinkedCancellationToken.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace Cloudtoid
+{
+    /// <summary>
+    /// This class provides a pool of <see cref="CancellationTokenSource"/>s that are used to create short-lived
+    /// linked token sources.
+    /// </summary>
+    public readonly struct LinkedCancellationToken : IEquatable<LinkedCancellationToken>, IDisposable
+    {
+        // internal for testing
+        internal static readonly int Count = Math.Min(Environment.ProcessorCount * 16, 256);
+
+        private static readonly Action<object?> CancelAction = s => ((CancellationTokenSource)s!).Cancel();
+        private static readonly CancellationTokenSource?[] Items;
+        private readonly CancellationTokenSource? source;
+        private readonly CancellationTokenRegistration reg1;
+        private readonly CancellationTokenRegistration reg2;
+
+        static LinkedCancellationToken()
+        {
+            Items = new CancellationTokenSource[Count];
+            for (var i = 0; i < Count; i++)
+                Items[i] = new CancellationTokenSource();
+        }
+
+        public LinkedCancellationToken(CancellationToken token1, CancellationToken token2)
+        {
+            var source = RentSource();
+            if (source is null)
+            {
+                source = CancellationTokenSource.CreateLinkedTokenSource(token1, token2);
+                reg1 = reg2 = default;
+            }
+            else
+            {
+                reg1 = token1.Register(CancelAction, source, false);
+                reg2 = token2.Register(CancelAction, source, false);
+            }
+
+            this.source = source;
+        }
+
+        public CancellationToken Token
+            => source?.Token ?? default;
+
+        public static bool operator ==(LinkedCancellationToken left, LinkedCancellationToken right)
+            => left.Equals(right);
+        public static bool operator !=(LinkedCancellationToken left, LinkedCancellationToken right)
+            => !left.Equals(right);
+
+        public override bool Equals(object? obj)
+            => obj is LinkedCancellationToken token && Equals(token);
+
+        public bool Equals(LinkedCancellationToken other)
+            => ReferenceEquals(source, other.source);
+
+        public override int GetHashCode()
+            => source?.GetHashCode() ?? 0;
+
+        public void Dispose()
+        {
+            if (source is null)
+                return;
+
+            if (reg1 == default)
+            {
+                source.Dispose();
+            }
+            else
+            {
+                reg1.Dispose();
+                reg2.Dispose();
+
+                if (source.IsCancellationRequested)
+                {
+                    source.Dispose();
+                    ReturnSource(new CancellationTokenSource());
+                }
+                else
+                {
+                    ReturnSource(source);
+                }
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static CancellationTokenSource? RentSource()
+        {
+            for (var i = 0; i < Count; i++)
+            {
+                while (true)
+                {
+                    var snapshot = Items[i];
+                    if (snapshot is null)
+                        break;
+
+                    if (Interlocked.CompareExchange(ref Items[i], null, snapshot) == snapshot)
+                        return snapshot;
+                }
+            }
+
+            return null;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static void ReturnSource(CancellationTokenSource source)
+        {
+            var i = 0;
+            while (true)
+            {
+                if (Items[i] is null)
+                {
+                    if (Interlocked.CompareExchange(ref Items[i], source, null) == null)
+                        return;
+                }
+                else
+                {
+                    if (ReferenceEquals(source, Items[i]))
+                        return;
+                }
+
+                i = (i + 1) % Count;
+            }
+        }
+    }
+}
---
+++ tests/Cloudtoid.Framework.UnitTests/Concurrency/LinkedCancellationTokenTests.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cloudtoid.Framework.UnitTests
+{
+    [TestClass]
+    public sealed class LinkedCancellationTokenTests
+    {
+        [TestMethod]
+        public void SuccessfulLinkedTokenCreation()
+        {
+            using var source1 = new CancellationTokenSource();
+            using var source2 = new CancellationTokenSource();
+            using var linked = new LinkedCancellationToken(source1.Token, source2.Token);
+            linked.Token.Should().NotBe(default(LinkedCancellationToken));
+        }
+
+        [TestMethod]
+        public void DisposeDefaultToken()
+        {
+            var linked = default(LinkedCancellationToken);
+            linked.GetHashCode().Should().Be(0);
+            linked.Token.Should().Be(default(CancellationToken));
+            linked.Dispose();
+        }
+
+        [TestMethod]
+        public void DisposeMoreThanOnce()
+        {
+            using var source1 = new CancellationTokenSource();
+            using var source2 = new CancellationTokenSource();
+            var linked = new LinkedCancellationToken(source1.Token, source2.Token);
+            linked.Dispose();
+            linked.Dispose();
+        }
+
+        [TestMethod]
+        public void CheckEquality()
+        {
+            using var source1 = new CancellationTokenSource();
+            using var source2 = new CancellationTokenSource();
+            using var linked = new LinkedCancellationToken(source1.Token, source2.Token);
+            linked.Equals(linked).Should().BeTrue();
+#pragma warning disable CS1718 // Comparison made to same variable
+            (linked == linked).Should().BeTrue();
+            (linked != linked).Should().BeFalse();
+#pragma warning restore CS1718 // Comparison made to same variable
+        }
+
+        [TestMethod]
+        public void ExhaustPool()
+        {
+            using var source1 = new CancellationTokenSource();
+            using var source2 = new CancellationTokenSource();
+            var count = LinkedCancellationToken.Count;
+            var set = new HashSet<LinkedCancellationToken>();
+
+            for (int i = 0; i < count + 10; i++)
+                set.Add(new LinkedCancellationToken(source1.Token, source2.Token)).Should().BeTrue();
+
+            foreach (var linked in set)
+                linked.Dispose();
+
+            for (int i = 0; i < count; i++)
+                set.Add(new LinkedCancellationToken(source1.Token, source2.Token)).Should().BeFalse();
+
+            for (int i = 0; i < 10; i++)
+                set.Add(new LinkedCancellationToken(source1.Token, source2.Token)).Should().BeTrue();
+
+            set.Should().HaveCount(count + 20);
+
+            foreach (var linked in set)
+                linked.Dispose();
+        }
+
+        [TestMethod]
+        public void CancelledNotReturned()
+        {
+            using var source1 = new CancellationTokenSource();
+            using var source2 = new CancellationTokenSource();
+            using var source3 = new CancellationTokenSource();
+            var count = LinkedCancellationToken.Count;
+            var set = new HashSet<LinkedCancellationToken>();
+
+            var cancelledLinked = new LinkedCancellationToken(source3.Token, source2.Token);
+            source3.Cancel();
+            cancelledLinked.Token.IsCancellationRequested.Should().BeTrue();
+            cancelledLinked.Dispose();
+
+            for (int i = 0; i < count; i++)
+            {
+                var linked = new LinkedCancellationToken(source1.Token, source2.Token);
+                set.Add(linked).Should().BeTrue();
+                cancelledLinked.Equals(linked).Should().BeFalse();
+            }
+
+            foreach (var linked in set)
+                linked.Dispose();
+        }
+    }
+}
